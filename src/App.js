73575b0276163c8a51397dc237ffb76e89c2c3a4
Amirata Khodaparast <amiratak88@gmail.com>
2018-11-23 20:30:13 -0500

    Add ActionCable component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Chatroom from './containers/chatroom'
+import { ActionCable } from 'react-actioncable-provider'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Chatroom />
+		<ActionCable
+			channel={{ channel: 'ClassroomsChannel', classroom_id: 1 }}
+			onReceived={console.log}
+		/>
       </div>
     );
   }
