d4013f82021b1b9aec2164dba113efc3d707630e
Amirata Khodaparast <amiratak88@gmail.com>
2018-11-23 20:25:18 -0500

    Set up ApplicationCableProvider

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux'
 import reducer from './reducers/index'
 import thunk from 'redux-thunk'
+import { ActionCableProvider } from 'react-actioncable-provider'
 
   let store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+	<ActionCableProvider url="ws://localhost:3000/cable">
+    	<App />
+	</ActionCableProvider>
   </Provider>
   , document.getElementById('root'));
