ac3b003ce51a43616466e602bedecff9df92029c
github username <bteng1226@gmail.com>
2018-11-23 21:24:26 -0500

    setting up for action cables

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux'
 import reducer from './reducers/index'
 import thunk from 'redux-thunk'
+import { ActionCableProvider } from 'react-actioncable-provider';
+import { API_WS_ROOT } from './constants';
+
 
   let store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+    <ActionCableProvider url={API_WS_ROOT}>
+      <App />
+    </ActionCableProvider>,
   , document.getElementById('root'));
