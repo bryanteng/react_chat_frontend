8a9505f81431f948f7cf41b500a31dc41a3484ae
github username <bteng1226@gmail.com>
2018-12-04 21:38:02 -0500

    added action and reducer

--- src/reducers/login.js
+++ src/reducers/login.js
@@ -1,10 +1,11 @@
-export default function loginReducer(state = {user_id: 1, username: "bryan"}, action){
+export default function loginReducer(state = {user_id: 0, username: ""}, action){
     switch (action.type) {
 
-      case 'SET_USER_ID':
+      case 'SET_USER':
         return {
           ...state,
-          user_id: action.payload
+          user_id: action.payload.user_id,
+          username: action.payload.username
         }
 
       case 'SET_USERNAME':
